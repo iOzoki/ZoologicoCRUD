6c8ac128a99801d3210ffa2056db5be33413f8d0
Isaac Adler <isaac.adler@ufrpe.br>
2025-05-09 12:48:05 -0300

    Nova classe abstrata para tratar funcionarios

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,4 +1,17 @@
 package model;
 
-public class Funcionario {
+public class Funcionario extends Pessoa{
+    private String cargo;
+
+    public Funcionario( int ID, String nome, int idade, String cargo){
+        super(ID, nome, idade);
+        this.cargo = cargo;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
 }
--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,4 +1,34 @@
 package model;
 
-public class Pessoa {
+public abstract class Pessoa {
+    int ID;
+    private String nome;
+    private int idade;
+
+    public Pessoa(int ID, String nome, int idade) {
+        this.ID = ID;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
 }
+
